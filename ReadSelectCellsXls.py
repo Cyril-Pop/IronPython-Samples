9de1800b3877bf3f62a0c039844601c239f4f480
Cyril.P <53660624+Cyril-Pop@users.noreply.github.com>
2020-05-22 20:20:12 +0200

    Update ReadSelectCellsXls.py

--- ReadSelectCellsXls.py
+++ ReadSelectCellsXls.py
@@ -24,35 +24,35 @@ from System.Runtime.InteropServices import Marshal
 refreshBoolean = IN[0]
 
 def checkInteger(val):
-	try:
-		check = int(val)
-		return True
-	except: return False	
+    try:
+        check = int(val)
+        return True
+    except: return False    
 
 class Lst_Xls():
-	def __init__(self):
-		try:
-			ex = System.Runtime.InteropServices.Marshal.GetActiveObject("Excel.Application")
-			ex.Visible = True
-			self.app = ex 
-			self.error = None
-			self.data = []
-			
-			workbook = ex.ActiveWorkbook
-			ws = ex.ActiveSheet
-			self.selectRange = ex.Selection 
-			for r in self.selectRange:
-				self.data.append(r.Value2)
-		except Exception as ex:
-			self.error   = str(ex)		
-objxls = Lst_Xls()	
+    def __init__(self):
+        try:
+            ex = System.Runtime.InteropServices.Marshal.GetActiveObject("Excel.Application")
+            ex.Visible = True
+            self.app = ex 
+            self.error = None
+            self.data = []
+            
+            workbook = ex.ActiveWorkbook
+            ws = ex.ActiveSheet
+            self.selectRange = ex.Selection 
+            for r in self.selectRange:
+                self.data.append(r.Value2)
+        except Exception as ex:
+            self.error   = str(ex)      
+objxls = Lst_Xls()  
 if objxls.error is None:
-	outElem = []
-	for val in objxls.data:
-		if checkInteger(val):
-			elemId = ElementId(int(val))
-			elem = doc.GetElement(elemId)
-			if elem is not None:
-				outElem.append(elem)
-	OUT = outElem
-else: OUT = None	
+    outElem = []
+    for val in objxls.data:
+        if checkInteger(val):
+            elemId = ElementId(int(val))
+            elem = doc.GetElement(elemId)
+            if elem is not None:
+                outElem.append(elem)
+    OUT = outElem
+else: OUT = None    
