12c9a4e72aded5281574a0bb6aafd6d9a25f2656
Cyril.P <53660624+Cyril-Pop@users.noreply.github.com>
2023-03-12 18:56:41 +0100

    Update and rename Import_Export_Xls_v4.0.py to Import_Export_Xls_v5.0.py

--- Excel Interop/Import_Export_Xls_v5.0.py
+++ Excel Interop/Import_Export_Xls_v5.0.py
@@ -35,10 +35,8 @@ class ExcelUtils():
 	def ConvertDataTableToArray(dataTable):
 		"""
 		This function converts a DataTable object to an Array.
-
 		:param dataTable: DataTable object to be converted
 		:type dataTable: DataTable
-
 		:return: Array of data from DataTable object
 		:rtype: Array
 		"""
@@ -66,13 +64,11 @@ class ExcelUtils():
 	def ExportXls(filepath, array_data, first_line_asHeader = True):
 		"""
 		This function exports data from an Array to an Excel workbook.
-
 		:param filepath: full path of out excel file
 		:type filepath: str
 		
 		:param array_data: Array of data to be exported
 		:type array_data: nested List or Array
-
 		:return: None
 		:rtype: None
 		"""
@@ -111,7 +107,6 @@ class ExcelUtils():
 	def ImportXls(filepath, lst_ColumnName = [], return_dataTable = False, sheetName = None):
 		"""
 		This function imports an excel file into a DataTable or a list of lists
-
 		:param filepath: full path of out excel file
 		:type filepath: str
 		
@@ -121,77 +116,97 @@ class ExcelUtils():
 		
 		:return: DataTable or a list of lists
 		"""
+		workbook = None
+		ws = None
+		datas = None
+		error = None
+		#
 		ex = Excel.ApplicationClass()
 		ex.Visible = False
 		lst_xls = []
-		workbook = ex.Workbooks.Open(filepath)
-		if sheetName is not None:
-			try:
-				ws = workbook.Worksheets[sheetName]
-			except:
+		try:
+			workbook = ex.Workbooks.Open(filepath)
+			if sheetName is not None:
+				try:
+					ws = workbook.Worksheets[sheetName]
+				except Exception as ex:
+					print(ex)
+					ws = workbook.Worksheets[1]
+			else:
 				ws = workbook.Worksheets[1]
-		else:
-			ws = workbook.Worksheets[1]
-		################################
-		##get number of Rows not empty ##
-		#################################
-		# get Rows Count method 1
-		# rowCountF = max(ws.Range(i).End(xlDirecUp).Row for i in ["A65536", "B65536", "C65536", "D65536", "E65536", "F65536", "G65536", "H65536"])
-		# get Rows Count method 2
-		# rowCountF = ws.Columns[1].End(xlDirecDown).Row
-		# get Rows Count method 3
-		#rowCountF=ws.UsedRange.Rows.Count
-		####################################
-		## get number of Columns not empty ##
-		#####################################
-		# get Columns Count method 1
-		# colCountF = max(ws.Range(i).End(xlDirecLeft).Column for i in ["ZZ1", "ZZ2", "ZZ3", "ZZ4", "ZZ5", "ZZ6", "ZZ7", "ZZ8", "ZZ9"])
-		# get Columns Count method 2
-		# colCountF = ws.Rows[1].End(xlDirecRight).Column
-		# get Columns Count method 3
-		#colCountF=ws.UsedRange.Columns.Count
-		#####################################
-		### other  method 2 maybe the best ###
-		######################################
-		last = ws.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, System.Type.Missing)
-		usedrange = ws.Range["A1", last]
-		rowCountF = last.Row
-		colCountF = last.Column
+			################################
+			##get number of Rows not empty ##
+			#################################
+			# get Rows Count method 1
+			# rowCountF = max(ws.Range(i).End(xlDirecUp).Row for i in ["A65536", "B65536", "C65536", "D65536", "E65536", "F65536", "G65536", "H65536"])
+			# get Rows Count method 2
+			# rowCountF = ws.Columns[1].End(xlDirecDown).Row
+			# get Rows Count method 3
+			#rowCountF=ws.UsedRange.Rows.Count
+			####################################
+			## get number of Columns not empty ##
+			#####################################
+			# get Columns Count method 1
+			# colCountF = max(ws.Range(i).End(xlDirecLeft).Column for i in ["ZZ1", "ZZ2", "ZZ3", "ZZ4", "ZZ5", "ZZ6", "ZZ7", "ZZ8", "ZZ9"])
+			# get Columns Count method 2
+			# colCountF = ws.Rows[1].End(xlDirecRight).Column
+			# get Columns Count method 3
+			#colCountF=ws.UsedRange.Columns.Count
+			#####################################
+			### other  method 2 maybe the best ###
+			######################################
+			last = ws.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, System.Type.Missing)
+			usedrange = ws.Range["A1", last]
+			rowCountF = last.Row
+			colCountF = last.Column
+			#print((rowCountF, colCountF))
+			#
+			fullrange = ws.Range[ws.Cells(1, 1), ws.Cells(rowCountF, colCountF)]
+			fullvalue = list(fullrange.Value2)
+			#split list into sublist with number of colum
+			n = colCountF					
+			datas = list(fullvalue[i:i+n] for i in range(0, len(fullvalue ), n))
+			# 
+			# convert to DataTable
+			dt = ExcelUtils.ConvertArrayToDataTable(datas)
+			# if lst_ColumnName is not empty remove other column by name
+			if len(lst_ColumnName) > 0:
+				# convert to DataView
+				dataView = dt.DefaultView
+				# re-convert to DataTable  with selection of Columns
+				dt = dataView.ToTable(False, System.Array[System.String](lst_ColumnName))
+			# overide data
+			if return_dataTable:
+				datas = dt
+			else:
+				datas = ExcelUtils.ConvertDataTableToArray(dt)
+		except Exception as excp:
+			import traceback
+			error = traceback.format_exc()
 		#
-		fullrange = ws.Range[ws.Cells(1, 1), ws.Cells(rowCountF, colCountF)]
-		fullvalue = list(fullrange.Value2)
-		#split list into sublist with number of colum
-		n = colCountF					
-		datas = list(fullvalue[i:i+n] for i in range(0, len(fullvalue ), n))
-		# 
-		# convert to DataTable
-		dt = ExcelUtils.ConvertArrayToDataTable(datas)
-		# if lst_ColumnName is not empty remove other column by name
-		if len(lst_ColumnName) > 0:
-			lst_columnName_to_remove = [col.ColumnName for col in dt.Columns if col.ColumnName not in lst_ColumnName]
-			for colName in lst_columnName_to_remove:
-				dt.Columns.Remove(colName)
-		# overide data
-		if return_dataTable:
-			datas = dt
-		else:
-			datas = ExcelUtils.ConvertDataTableToArray(dt)
-		ex.Workbooks.Close()
-		ex.Quit()
+		# close excel properly
+		if ex is not None:
+			ex.Workbooks.Close()
+			ex.Quit()
 		#other proper way to make sure that you really closed and released all COM objects 
 		if workbook is not None:
 			Marshal.ReleaseComObject(workbook)
 		if ex is not None:
-			Marshal.ReleaseComObject(ex)     
-		workbook = None        
+			Marshal.ReleaseComObject(ex)
+		workbook = None
 		ex = None
-		return datas
+		if error is not None:
+			return error
+		else:
+			return datas
 		
 fileapth = IN[0]
-datas = ExcelUtils.ImportXls(fileapth, lst_ColumnName = ["Employee ID","Full Name","Job Title"], return_dataTable = False)
+#datas = ExcelUtils.ImportXls(fileapth, lst_ColumnName = ["Employee ID","Full Name","Job Title"], return_dataTable = False)
+datas = ExcelUtils.ImportXls(fileapth, return_dataTable = False, lst_ColumnName = ["Element type Name", "Layer Name"], sheetName = "Non parameters")
+# if return_dataTable = True
 if isinstance(datas, DataTable):
 	OUT = [c.ColumnName  for c in datas.Columns]
 else:
-	new_file_path = fileapth.replace(".xlsx", "_v2.xlsx")
-	ExcelUtils.ExportXls(new_file_path, datas, first_line_asHeader = True)
+	# new_file_path = fileapth.replace(".xlsx", "_v2.xlsx")
+	# export_datas = ExcelUtils.ExportXls(new_file_path, datas, first_line_asHeader = False)
 	OUT = datas
